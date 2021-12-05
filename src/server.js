ff6db35c3769c0a8618e138c35e17a2b0f1b3840
unknown <danielbon29@gmail.com>
2021-12-05 16:43:30 -0500

    App commited v1

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //set view engine
 app.set('view engine','ejs');
-// app.set('views',path.resolve(__dirname,'views/ejs'));
+app.set('views',path.resolve(__dirname,'views'));
 
 //load assets
 app.use('/css',express.static(path.resolve(__dirname,'assets/css')));
