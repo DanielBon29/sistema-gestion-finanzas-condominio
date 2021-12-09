a493112bb4a861796fa9c1c9168eb9e95a5512f6
unknown <danielbon29@gmail.com>
2021-12-08 21:08:21 -0500

    App commited

--- src/server/database/connection.js
+++ src/server/database/connection.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async() => {
     try{
         //mongodb connection string
-        // const con = await mongoose.connect(process.env.MONGO_URI);
-        const con = await mongoose.connect('mongodb+srv://admin-db:ubYuviD4dJn5etX@tesoreria.peoii.mongodb.net/Tesoreria?retryWrites=true&w=majority');
-        
+        const con = await mongoose.connect(process.env.MONGO_URI);        
         console.log(`Mongo DB connected: ${con.connection.host}`);
     }catch(err){
         console.log(err);
